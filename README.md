8080226be1bca825ad2b4c3d4415848b9d4d87e9
rolandopalermo <rolando.roc@gmail.com>
2026-02-01 12:24:52 -0600

    Enhance envioComprobante flow

---
+++ README.md
@@ -0,0 +1,3 @@
+- Integration tests
+
+mvn -Dsri.it.enabled=true -Dtest=SriRecepcionClientIT test
--- src/main/java/ec/veronica/soap/client/SriEnvironment.java
+++ src/main/java/ec/veronica/soap/client/SriEnvironment.java
@@ -1,6 +1,28 @@
 package ec.veronica.soap.client;
 
 public enum SriEnvironment {
-    PRUEBAS,
-    PRODUCCION
+    PRUEBAS(1),
+    PRODUCCION(2);
+
+    private final int code;
+
+    SriEnvironment(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static SriEnvironment fromCode(int code) {
+        for (SriEnvironment env : values()) {
+            if (env.code == code) {
+                return env;
+            }
+        }
+        throw new IllegalArgumentException(
+                "Unsupported SRI environment code: " + code +
+                        ". Supported codes are: 1 (PRUEBAS), 2 (PRODUCCION)"
+        );
+    }
 }
